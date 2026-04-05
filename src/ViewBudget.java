52a6df7e2bdf3c44b46d7ea304d3e26874d7b53e
denisesabando21-dotcom <denisesabando21@gmail.com>
2026-04-05 21:58:25 +0800

    Update ViewBudget.java

--- src/ViewBudget.java
+++ src/ViewBudget.java
@@ -199,3 +199,19 @@ public class ViewBudget {
         }
     }
 }
+
+class Session {
+    private static String currentUser = null;
+
+    public static void login(String username) {
+        currentUser = username;
+    }
+
+    public static void logout() {
+        currentUser = null;
+    }
+
+    public static String getCurrentUser() {
+        return currentUser;
+    }
+}
